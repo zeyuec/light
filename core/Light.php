8d55c035b8a17f030baea45c5f63f75d0b537e74
zeyuec <zeyuec@gmail.com>
2013-02-08 02:20:06 +0800

    fix redirect bug

--- core/Light.php
+++ core/Light.php
@@ -33,7 +33,7 @@ class Light
     // db
     const DB_HOST = '127.0.0.1';
     const DB_PORT = '3306';
-    const DB_NAME = 'light_blog';
+    const DB_NAME = 'light';
     const DB_USER = 'root';
     const DB_PASS = '123456';
 
